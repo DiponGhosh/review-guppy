347dc1ee89f5c76bd38adc9284ae867295e648f5
DiponGhosh <diponghosh0@gmail.com>
2018-03-12 23:47:49 +0530

    an ui update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -5,6 +6,7 @@ from flask import request
 from amzallscrp import Amzallscrp
 from predictor import Predictor
 from productname import Productname
+from pieResult import PieResult
 from imgLink import ImgLink
 
 app = Flask(__name__)
@@ -27,6 +29,7 @@ def contact():
 
 @app.route('/result', methods = ['GET'])
 def make_decision():
+	os.remove('./static/images/result_pie.png')
 	product_url = request.args.get('product_url')
 
 	productName = Productname(product_url)
@@ -34,7 +37,7 @@ def make_decision():
 		return '<h1>Connection Error</h1> <br/> <h3>Unstable Connection</h3>'
 
 	imageLink = ImgLink(product_url)
-	if not productName:
+	if not imageLink:
 		return '<h1>Connection Error</h1> <br/> <h3>Unstable Connection</h3>'
 	
 	reviews = Amzallscrp(product_url)
@@ -49,14 +52,22 @@ def make_decision():
 
 	list_result = Predictor(reviews)
 
+	PieResult(list_result)
+
 	positive_logo = '../static/images/positive.png'
 	neutral_logo = '../static/images/neutral.png'
 	negative_logo = '../static/images/negative.png'
+	result_pie_src = '../static/images/result_pie.png'
 
 	#check condition
-	result_logo_src = positive_logo
+	result_logo_src = negative_logo
+	result_text = "purchase it"
 	
-	return render_template('result.html', productName = productName, rev = str(len(reviews)), pos = list_result[0], neg = list_result[1], neu = list_result[2])
+	return render_template('decision.html', productName = productName, product_url=product_url,
+							rev = str(len(reviews)), 
+							pos = list_result[0], neg = list_result[1], neu = list_result[2],
+							product_image_src=imageLink, result_logo_src=result_logo_src,
+							result_pie_src=result_pie_src, result_text=result_text)
 	
 
 if __name__ == '__main__':
---
+++ pieResult.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+def PieResult(sizes):
+    labels = ['positive', 'neutral', 'negative']
+    colors = ['#2E7D32', '#F57C00', '#D32F2F']
+
+    plt.title('Review Analysis')
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%',
+            shadow=False, startangle=90)
+
+
+    centre_circle = plt.Circle((0,0),0.70,fc='white')
+    fig = plt.gcf()
+    fig.gca().add_artist(centre_circle)
+
+    # Equal aspect ratio ensures that pie is drawn as a circle
+    ax1.axis('equal')
+    plt.tight_layout()
+    #plt.show()
+    plt.savefig('./static/images/result_pie.png')
+
+#list_reult = ['75.00', '5.00', '20.00']
+#PieResult(list_reult)
