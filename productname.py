4eac676bf3994daa4f1ccb71f19e9c9b86a78c9f
DiponGhosh <diponghosh0@gmail.com>
2018-03-13 12:51:35 +0530

    scrapping fixes try 1

--- productname.py
+++ productname.py
@@ -10,9 +10,15 @@ def getProductName(url):
 	r = requests.get(url)
 	soup = BeautifulSoup(r.content, "lxml")
 
-	prod_title = soup.find_all("span", {"id": "productTitle"})
-
+	prod_title = soup.find("span", {"id": "productTitle"})
+	'''
 	for item in prod_title:
 		productName = item.text
+	'''
+	productName = prod_title.text
 
 	return productName
+
+
+#url = "https://www.amazon.in/Bose-Companion-III-Multimedia-Speakers/dp/B011KSVFV8/ref=sr_1_10?s=electronics&ie=UTF8&qid=1520917726&sr=1-10&keywords=bose"
+#print(Productname(url))
